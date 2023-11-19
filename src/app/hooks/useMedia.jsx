c1478091956f0cf134eeca919f60860941be6864
flgisimone <florestagiuliosimone@gmail.com>
2023-11-19 10:19:24 +0100

    v2.1.3

--- src/app/hooks/useMedia.jsx
+++ src/app/hooks/useMedia.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 
-const useMedia = (window: any) => {
+const useMedia = () => {
     
     const [isMobile, setIsMobile] = useState(window?.innerWidth < 768);
 
